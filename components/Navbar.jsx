4dfaa9dc4f66bc67924895af4312ebd6b8cf7fe2
unknown <vibolreach.m.ouk@gmail.com>
2023-02-11 20:39:27 -0500

    fixed:darkmode

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,6 @@ const Navbar = () => {
     const handleNav = () => {
         setNav(!nav);
     }
-    const [darkMode, setDarkMode] = useState(false);
     return (
         <div className='fixed w-full h-20 shadow-xl z-[100] '>
             <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>               
