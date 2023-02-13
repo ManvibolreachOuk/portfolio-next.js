6ea695d91f731ddaa6c1003045c11855d4625da9
unknown <vibolreach.m.ouk@gmail.com>
2023-02-13 14:32:37 -0500

    initial test

--- components/Main.jsx
+++ components/Main.jsx
@@ -10,9 +10,9 @@ const Main = () => {
         <div className='w-full h-screen text-center'>
             <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
                 <div>                                          
-                    <p className='uppercase text-sm tracking-widest text-gray-600 pt-40'>LET'S HAVE A GREAT IMPACT TOGETHER</p> 
+                    <p className='uppercase text-sm tracking-widest text-gray-600 pt-40'>LET&#39;S HAVE A GREAT IMPACT TOGETHER</p> 
                     <h2 className='py-4 text-gray-700'>
-                        Hey, I'm <span className='text-[#5651e5]'>Reach</span>
+                        Hey, I&#39;m <span className='text-[#5651e5]'>Reach</span>
                     </h2>
                     <h2 className='py-2 text-gray-700'>Web Developer And Designer</h2>
                     <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
